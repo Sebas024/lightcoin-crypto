35edba4699caab582a0c6ae9bfb00ec431a8685e
Charles Samonte <charles.deo024@hotmail.ca>
2023-06-09 23:17:13 -0600

    updated

--- index.js
+++ index.js
@@ -1,16 +1,59 @@
 let balance = 500.00;
 
-class Withdrawal {
 
-  constructor(amount) {
-    this.amount = amount;
-  }
 
-  commit() {
-    balance -= this.amount;
+
+class Transaction {
+
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
   }
 
 }
+
+
+class Deposit {
+
+  constructor(amount) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+  commit() {
+    this.account.balance += this.amount;
+  }
+
+}
+
+class Withdrawal {
+
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+  commit() {
+    this.account.balance -= this.amount;
+  }
+
+  
+
+}  
+
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    // Have the account balance start at $0 since that makes more sense.
+    this.balance = 0;
+  }
+
+}
+const myAccount = new Account("snow-patrol");
+
+
+
 
 
 
@@ -26,4 +69,66 @@ t2 = new Withdrawal(9.99);
 t2.commit();
 console.log('Transaction 2:', t2);
 
+// t3 = new Deposit(120.00);
+// t3.commit();
+// console.log('Transaction 3:', t3);
+
+t1 = new Withdrawal(50.25, myAccount);
+t1.commit();
+
 console.log('Balance:', balance);
+
+
+
+
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
+
+}
+
+class Transaction {
+
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
+  }
+
+  commit() {
+    this.account.balance += this.value;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
+  }
+
+}
+
+// DRIVER CODE BELOW
+
+const myAccount = new Account('billybob');
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
+t1.commit();
+
+const t2 = new Withdrawal(50.00, myAccount);
+t2.commit();
+
+console.log('Ending Balance:', myAccount.balance);
